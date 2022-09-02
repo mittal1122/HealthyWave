a0ab0192b49e1b9339fd9d62232d082b18a821ce
Vipul Prajapati <vipul1551>
2022-09-02 16:55:39 +0530

    modified

--- src/main/java/com/controller/PatientDocController.java
+++ src/main/java/com/controller/PatientDocController.java
@@ -22,63 +22,64 @@ import com.repository.PatientDocRepository;
 @RestController
 public class PatientDocController {
 
-	@Autowired
-	PatientDocRepository patientDocRepo;
-	
-	@PostMapping("/patientDocument")
-	public ResponseEntity<?> addPDoc(@RequestBody PatientDocumentBean patientDocBean){
-		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
-		patientDocRepo.save(patientDocBean);
-		return ResponseEntity.status(HttpStatus.OK).body(resp);
-	}
-	@GetMapping("/patientDocument")
-	public ResponseEntity<?> allPDoc(){
-		ResponseBean<List<PatientDocumentBean>> resp = new ResponseBean<>();
-		List<PatientDocumentBean> pDoc = patientDocRepo.findAll();
-		resp.setData(pDoc);
-		resp.setMsg("Patient Document List...");
-		return ResponseEntity.status(HttpStatus.OK).body(resp);
-	}
-	
-	@GetMapping("/patientDocument")
-	public ResponseEntity<?> getPDocByDocId(@PathVariable("pDocId") UUID pDocid){
-		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
-		Optional<PatientDocumentBean> pDoc = patientDocRepo.findById(pDocid);
-		resp.setData(pDoc.get());
-		resp.setMsg("Patient Document Details...");
-		return ResponseEntity.status(HttpStatus.OK).body(resp);
-	}
-
-	@GetMapping("/patientDocument")
-	public ResponseEntity<?> getPDocByPatientId(@PathVariable("patientId") UUID patientid){
-		ResponseBean<List<PatientDocumentBean>> resp = new ResponseBean<>();
-		List<PatientDocumentBean> pDoc = patientDocRepo.findByPatientId(patientid);
-		resp.setData(pDoc);
-		resp.setMsg("Patient Document Details...");
-		return ResponseEntity.status(HttpStatus.OK).body(resp);
-	}
-	
-	@DeleteMapping("/patientDocument/{pDocId}")
-	public ResponseEntity<?> deletePDoc(@PathVariable("pDocId") UUID pDocid){
-		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
-		Optional<PatientDocumentBean> pDoc = patientDocRepo.findById(pDocid);
-		if(pDoc.isPresent()) {
-			patientDocRepo.deleteById(pDocid);
-			resp.setData(pDoc.get());
-			resp.setMsg("Patient Document Deleted...");
-			return ResponseEntity.status(HttpStatus.OK).body(resp);
-		}else {
-			resp.setMsg("Patient Document not Deleted");
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
-		}
-	}
-
-	
-	@PutMapping("/patientDocument")
-	public ResponseEntity<?> updatePDoc(@RequestBody PatientDocumentBean patientDocBean){
-		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
-		patientDocRepo.save(patientDocBean);
-		return ResponseEntity.status(HttpStatus.OK).body(resp);
-	}
+//	@Autowired
+//	PatientDocRepository patientDocRepo;
+//	
+//	@PostMapping("/patientDocument")
+//	public ResponseEntity<?> addPDoc(@RequestBody PatientDocumentBean patientDocBean){
+//		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
+//		patientDocRepo.save(patientDocBean);
+//		return ResponseEntity.status(HttpStatus.OK).body(resp);
+//	}
+//	
+//	@GetMapping("/patientDocument")
+//	public ResponseEntity<?> allPDoc(){
+//		ResponseBean<List<PatientDocumentBean>> resp = new ResponseBean<>();
+//		List<PatientDocumentBean> pDoc = patientDocRepo.findAll();
+//		resp.setData(pDoc);
+//		resp.setMsg("Patient Document List...");
+//		return ResponseEntity.status(HttpStatus.OK).body(resp);
+//	}
+//	
+//	@GetMapping("/patientDocument")
+//	public ResponseEntity<?> getPDocByDocId(@PathVariable("pDocId") UUID pDocid){
+//		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
+//		Optional<PatientDocumentBean> pDoc = patientDocRepo.findById(pDocid);
+//		resp.setData(pDoc.get());
+//		resp.setMsg("Patient Document Details...");
+//		return ResponseEntity.status(HttpStatus.OK).body(resp);
+//	}
+//
+//	@GetMapping("/patientDocument")
+//	public ResponseEntity<?> getPDocByPatientId(@PathVariable("patientId") UUID patientid){
+//		ResponseBean<List<PatientDocumentBean>> resp = new ResponseBean<>();
+//		List<PatientDocumentBean> pDoc = patientDocRepo.findByPatientId(patientid);
+//		resp.setData(pDoc);
+//		resp.setMsg("Patient Document Details...");
+//		return ResponseEntity.status(HttpStatus.OK).body(resp);
+//	}
+//	
+//	@DeleteMapping("/patientDocument/{pDocId}")
+//	public ResponseEntity<?> deletePDoc(@PathVariable("pDocId") UUID pDocid){
+//		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
+//		Optional<PatientDocumentBean> pDoc = patientDocRepo.findById(pDocid);
+//		if(pDoc.isPresent()) {
+//			patientDocRepo.deleteById(pDocid);
+//			resp.setData(pDoc.get());
+//			resp.setMsg("Patient Document Deleted...");
+//			return ResponseEntity.status(HttpStatus.OK).body(resp);
+//		}else {
+//			resp.setMsg("Patient Document not Deleted");
+//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
+//		}
+//	}
+//
+//	
+//	@PutMapping("/patientDocument")
+//	public ResponseEntity<?> updatePDoc(@RequestBody PatientDocumentBean patientDocBean){
+//		ResponseBean<PatientDocumentBean> resp = new ResponseBean<>();
+//		patientDocRepo.save(patientDocBean);
+//		return ResponseEntity.status(HttpStatus.OK).body(resp);
+//	}
 
 }
