9ea76c2bc3a9c21cd68167a0911c0f0b853c13ce
mittal1122 <mittalkumavat6676@gmail.com>
2022-09-11 21:50:54 +0530

    slot controller and repository

--- src/main/java/com/repository/SlotRepository.java
+++ src/main/java/com/repository/SlotRepository.java
@@ -1,11 +1,17 @@
 package com.repository;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bean.SlotBean;
 
 public interface SlotRepository extends CrudRepository<SlotBean, UUID>{
+//	@Query(value="select * from where ")
+//	List<SlotBean> findAllById(UUID doctorId);
 
+	@Query(value = "select * from slot where doctor_id = :doctorId and date = :dateData",nativeQuery = true)
+	List<SlotBean> findByDoctorIdDate(UUID doctorId,String dateData);
 }
